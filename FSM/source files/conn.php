154a5e27759007ef2727de309270f4855257605f
SAGAR M S <152598291+sagarongit24@users.noreply.github.com>
2024-02-17 11:16:50 +0530

    Update conn.php

--- FSM/source files/conn.php
+++ FSM/source files/conn.php
@@ -1,13 +1,11 @@
 <?php
-$servername = "localhost"; // Replace with your database server name or IP address
-$username = "root"; // Replace with your MySQL username
-$password = ""; // Replace with your MySQL password
-$database = "film_studio"; // Replace with your database name
+$servername = "localhost"; 
+$username = "root";
+$password = ""; 
+$database = "film_studio"; 
 
-// Create a connection to the MySQL database
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Check if the connection was successful
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
